7a38619b55db25e1a97bf12c426eb4c3d8d44bc0
mr_alex <sirotaalexandr30@gmail.com>
2020-02-20 21:16:42 +0200

    added files and some implement of parser

---
+++ HashCode/HashCode/DataParser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace HashCode
+{
+    public static class DataParser
+    {
+        public static async void ParseData()
+        {
+            const string pathA = @"/Users/ninja/Projects/HashCode/Data/a_example.txt";
+            const string fileB = @"/Users/ninja/Projects/HashCode/Data/b_read_on.txt";
+            const string fileC = @"/Users/ninja/Projects/HashCode/Data/c_incunabula.txt";
+            const string fileD = @"/Users/ninja/Projects/HashCode/Data/d_tough_choices.txt";
+            const string fileE = @"/Users/ninja/Projects/HashCode/Data/e_so_many_books.txt";
+            const string fileF = @"/Users/ninja/Projects/HashCode/Data/f_libraries_of_the_world.txt";
+
+
+        }
+
+        public static async Task StartParseAsync(string path)
+        {
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                string line = string.Empty;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    
+                }
+            }
+        }
+    }
+}
